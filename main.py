f4cca5f1fb8d193f22d997acb29b0d8960d23228
ryannicoletti <r.nicoletti1@gmail.com>
2021-12-31 12:35:29 -0500

    cleaned up main file and removed old code

--- main.py
+++ main.py
@@ -5,42 +5,7 @@ from game_states.menu import Menu
 from game_states.game_over import GameOver
 from game import GameRunner
 
-#FPS = 60
-
-#WIN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('Hasami Shogi')
-
-
-# def get_row_col_from_mouse(pos):
-#     x, y = pos
-#     row = y // SQUARE_SIZE  # invert, SQUARE_SIZE = WIDTH//COLS, width=800, cols = 9
-#     col = x // SQUARE_SIZE
-#     return row, col
-
-
-# def main():
-#     run = True
-#     clock = pg.time.Clock()
-#     game = Hasamishogigame(WIN)
-#     while run:
-#         clock.tick(FPS)
-#
-#         for event in pg.event.get():
-#             if event.type == pg.QUIT:
-#                 run = False
-#             if event.type == pg.MOUSEBUTTONDOWN:
-#                 pos = pg.mouse.get_pos()
-#                 row, col = get_row_col_from_mouse(pos)
-#                 game.select_move(row, col)
-#
-#         game.update()
-#     pg.quit()
-#
-#
-# main()
-
-
-# state machine code
 
 pg.init()
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
