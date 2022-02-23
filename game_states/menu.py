46bb8d3cc52f237f20955cd31c0882987cbb34cc
ryannicoletti <r.nicoletti1@gmail.com>
2022-02-23 10:32:30 -0500

    added mouse hover and mouse button down events to menu page

--- game_states/menu.py
+++ game_states/menu.py
@@ -10,6 +10,8 @@ class Menu(BaseState):
         self.active_index = 0
         self.options = ["Start Game", "Quit Game"]
         self.next_state = "GAMEPLAY"
+        self.start_rect = self.get_text_positions(self.render_text(0), 0)
+        self.quit_rect = self.get_text_positions(self.render_text(1), 1)
 
     def render_text(self, index):
         color = RED if index == self.active_index else WHITE
@@ -26,8 +28,10 @@ class Menu(BaseState):
             self.quit = True
 
     def get_event(self, event):
+
         if event.type == pg.QUIT:
             self.quit = True
+
         elif event.type == pg.KEYUP:
             if event.key == pg.K_UP:
                 self.active_index = 1 if self.active_index <= 0 else 0
@@ -36,8 +40,25 @@ class Menu(BaseState):
             elif event.key == pg.K_RETURN:
                 self.handle_action()
 
+        elif event.type == pg.MOUSEMOTION:
+            if self.start_rect.collidepoint(pg.mouse.get_pos()):
+                self.active_index = 0
+            elif self.quit_rect.collidepoint(pg.mouse.get_pos()):
+                self.active_index = 1
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            if self.start_rect.collidepoint(pg.mouse.get_pos()) or self.quit_rect.collidepoint(pg.mouse.get_pos()):
+                self.handle_action()
+
     def draw(self, surface):
         surface.fill(BLACK)
         for index, option in enumerate(self.options):
             text_render = self.render_text(index)
             surface.blit(text_render, self.get_text_positions(text_render, index))
+
+    def get_start_rect(self):
+        text_render = self.render_text(0)
+        return text_render.get_rect()
+
+    def get_quit_rect(self):
+        text_render = self.render_text(1)
+        return text_render.get_rect()
